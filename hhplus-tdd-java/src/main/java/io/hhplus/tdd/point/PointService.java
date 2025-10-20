d59fe74eca81ec81b1da49910b9ee438677b392a
eri <44278562+elyo9381@users.noreply.github.com>
2025-10-20 20:50:54 +0900

    refactoring : PointSer vice의 charge() , use() 중복 제거 및 updatePoint() 생성

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointService.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/point/PointService.java
@@ -39,27 +39,24 @@ public class PointService {
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
     public UserPoint charge(long id, long amount) {
-        UserPoint findUserPoint = userPointTable.selectById(id);
-
-        UserPoint calculatedPoint = findUserPoint.calculatePoint(amount, TransactionType.CHARGE);
-
-        UserPoint updateUserPoint = userPointTable.insertOrUpdate(calculatedPoint.id(), calculatedPoint.point());
-        pointHistoryTable.insert(id, amount,TransactionType.CHARGE, updateUserPoint.updateMillis());
-
-        return updateUserPoint;
+        return updatePoint(id, amount, TransactionType.CHARGE);
     }
 
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
     public UserPoint use(long id, long amount) {
-        UserPoint userPoint = userPointTable.selectById(id);
-        UserPoint calculatedUserPoint = userPoint.calculatePoint(amount, TransactionType.USE);
+        return updatePoint(id, amount, TransactionType.USE);
+    }
 
-        UserPoint updateUserPoint = userPointTable.insertOrUpdate(userPoint.id(), calculatedUserPoint.point());
-        pointHistoryTable.insert(id, amount,TransactionType.USE, updateUserPoint.updateMillis());
+    private UserPoint updatePoint(long id, long amount, TransactionType type) {
+        UserPoint findUserPoint = userPointTable.selectById(id);
+
+        UserPoint calculatedPoint = findUserPoint.calculatePoint(amount, type);
+
+        UserPoint updateUserPoint = userPointTable.insertOrUpdate(calculatedPoint.id(), calculatedPoint.point());
+        pointHistoryTable.insert(id, amount, type, updateUserPoint.updateMillis());
 
         return updateUserPoint;
-
     }
 }
